0e34a84af26ab6bdd6b8e2905426d70feac8cc74
Edmore Moyo <edmore@inclusivitysolutions.com>
2017-06-28 13:41:19 +0200

    Add validations for goal attributes

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
+
+  setup do
+    @goal = goals(:one)
+    @invalid_goal = goals(:invalid)
+    @invalid_score1 = goals(:invalid_score1)
+    @invalid_score2 = goals(:invalid_score2)
+  end
+
+  test 'valid goal' do
+    assert @goal.valid?
+  end
+
   test 'title is required' do
-    assert true
+    refute @invalid_goal.valid?
   end
 
   test 'score is between 1 and 10' do
-    assert true
+    refute @invalid_score1.valid?
+    refute @invalid_score2.valid?
   end
 end
